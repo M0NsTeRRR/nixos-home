8682a611355adbb6e6bbf8b6e00deae9cf5e5497
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-08-12 00:18:13 +0200

    fix: boot

--- modules/boot/default.nix
+++ modules/boot/default.nix
@@ -1,6 +1,9 @@
 {
   boot.loader = {
-    systemd-boot.enable = true;
+    # systemd-boot.enable = true;
     efi.canTouchEfiVariables = true;
+    grub.devices = [
+      "/dev/disk/by-id/ata-VBOX_HARDDISK_VBd954b569-0a9098bd"
+    ];
   };
 }
