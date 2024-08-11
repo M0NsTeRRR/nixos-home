b1c420e891fdfa1df86b252020772d810bfa4ebf
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-08-11 19:05:25 +0200

    fix: nixos module

--- modules/default.nix
+++ modules/default.nix
@@ -4,7 +4,6 @@
     ./docker
     ./gpg
     ./internationalization
-    ./polkit
     ./printer
     ./security
     ./ssh
--- modules/security/default.nix
+++ modules/security/default.nix
@@ -1,3 +1,4 @@
+{ pkgs, username, ... }:
 {
   boot.kernel.sysctl = {
     # ANSSI R9
@@ -35,5 +36,14 @@
   
   security = {
     sudo.wheelNeedsPassword = false;
+    polkit = {
+      enable = true;
+      extraConfig = ''
+        /* Log authorization checks. */
+        polkit.addRule(function(action, subject) {
+          polkit.log("user " +  subject.user + " is attempting action " + action.id + " from PID " + subject.pid);
+        });       
+      '';
+    };
   };
 }
