b90d14f00b5f0c20e5227588c39ebd03a7d30c82
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-08-14 00:14:05 +0200

    fix: zsh config

--- modules/terminal/default.nix
+++ modules/terminal/default.nix
@@ -15,18 +15,5 @@
     shellAliases = {
       k = "kubectl";
     };
-
-    plugins = with pkgs; [
-      {
-        name = "zsh-syntax-highlighting";
-        src = fetchFromGitHub {
-          owner = "zsh-users";
-          repo = "zsh-syntax-highlighting";
-          rev = "0.8.0";
-          sha256 = "1yl8zdip1z9inp280sfa5byjbf2vqh2iazsycar987khjsi5d5w8";
-        };
-        file = "zsh-syntax-highlighting.zsh";
-      }
-    ];
   };
 }
