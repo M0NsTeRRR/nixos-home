ecc5da1aa4a3b4d63b7e61ebe8225faa696e6bd4
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-08-14 13:26:31 +0200

    fix: configure hyprpaper as services

--- modules/hypr/hyprpaper/default.nix
+++ modules/hypr/hyprpaper/default.nix
@@ -1,12 +1,14 @@
 { username, ... }:
 {
-  services.hyprpaper.settings = {
-    preload = [
-      "/home/${username}/.config/backgrounds/scifi_landscape.jpg"
-    ];
-
-    wallpaper = [
-      "monitor,/home/${username}/.config/backgrounds/scifi_landscape.jpg"
-    ];
+  services.hypridle = {
+    enable = true;
+    settings = {
+      preload = [
+        "/home/${username}/.config/backgrounds/scifi_landscape.jpg"
+      ];
+      wallpaper = [
+        "monitor,/home/${username}/.config/backgrounds/scifi_landscape.jpg"
+      ];
+    };
   };
 }
