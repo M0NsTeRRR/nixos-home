408793d12c7f2a419e85fa8a3a3204d1ed77b754
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-08-14 11:25:50 +0200

    fix: cert hash

--- modules/ca-certificates/default.nix
+++ modules/ca-certificates/default.nix
@@ -2,8 +2,8 @@
 let
   # Unicornafk
   unicornafkCert = pkgs.fetchurl {
-    url = "https://github.com/M0NsTeRRR/homelabv3-infra/blob/main/ssl/unicornafk.crt";
-    sha256 = "13fdl3kqq39s9ixfbhv3hk7ggyygjg6lh1ls2js0q7hm87j9mp9f";
+    url = "https://raw.githubusercontent.com/M0NsTeRRR/homelabv3-infra/main/ssl/unicornafk.crt";
+    sha256 = "jdwZkCSiJwr8Ce2ut55g6x9F0SJY0ddNvturQhkzYiE=";
   };
 in
 {
