cd5fb69dfc66468daa4bafd01564aa5d0857b60f
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-09-13 02:20:09 +0200

    fix: rofi

--- home/user/environment.nix
+++ home/user/environment.nix
@@ -1,18 +1,5 @@
 {
   home.sessionVariables = {
-    NIXOS_OZONE_WL = "1";
-    WLR_RENDERER_ALLOW_SOFTWARE = "1";
-
-    # https://wiki.hyprland.org/Configuring/Environment-variables/
-    XDG_CURRENT_DESKTOP = "Hyprland";
-    XDG_SESSION_TYPE = "wayland";
-    XDG_SESSION_DESKTOP = "Hyprland";
-    GDK_BACKEND = "wayland";
-    QT_QPA_PLATFORM = "wayland";
-    QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
-    QT_AUTO_SCREEN_SCALE_FACTOR = "1";
-    SDL_VIDEODRIVER = "wayland";
-
     TERMINAL = "kitty";
     MOZ_ENABLE_WAYLAND = "1";
 
