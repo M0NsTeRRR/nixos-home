75c432a9c24d552c02e3cff0c01e333ef88e3dcc
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-10-22 23:30:46 +0200

    feat: update hyprpanel config

--- home/user/programs/git.nix
+++ home/user/programs/git.nix
@@ -3,5 +3,14 @@
     enable = true;
     userName = "Ludovic Ortega";
     userEmail = "ludovic.ortega@adminafk.fr";
+    signing = {
+      key = "81E390A404C7D583A6D6958E22F86C9B1BEC401D";
+      signByDefault = true;
+    };
+    extraConfig = {
+      core.editor = "vim";
+      ui.color = true;
+      push.autoSetupRemote = true;
+    };
   };
 }
