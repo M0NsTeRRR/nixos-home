04adcc5b330f36aab140ead84f13ea01a1962c8c
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2024-12-26 16:51:14 +0100

    feat: migrate from unstable to stable for ssdm astronaut
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- system/modules/display-manager.nix
+++ system/modules/display-manager.nix
@@ -1,14 +1,4 @@
-{ pkgs-unstable, ... }:
-let
-  sddm-astronaut = pkgs-unstable.sddm-astronaut.override {
-    themeConfig = {
-      AccentColor = "#B2D0E2";
-      FormPosition = "left";
-
-      ForceHideCompletePassword = true;
-    };
-  };
-in
+{ pkgs, ... }:
 {
   programs.hyprland.enable = true;
 
@@ -18,13 +8,20 @@ in
       enable = true;
       autoNumlock = true;
       # needed for sddm theme (qt6 sddm version)
-      package = pkgs-unstable.kdePackages.sddm;
+      package = pkgs.kdePackages.sddm;
       theme = "sddm-astronaut-theme";
-      extraPackages = [ sddm-astronaut ];
       enableHidpi = true;
       wayland.enable = true;
     };
   };
 
-  environment.systemPackages = [ sddm-astronaut ];
+  environment.systemPackages = [
+    (pkgs.sddm-astronaut.override {
+      themeConfig = {
+        AccentColor = "#B2D0E2";
+        FormPosition = "left";
+        ForceHideCompletePassword = true;
+      };
+    })
+  ];
 }
