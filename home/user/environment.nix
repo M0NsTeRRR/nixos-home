880e9a893a52c74efe0359cc6b30200cc420f328
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2025-01-26 03:06:19 +0100

    feat: setup gaming
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- home/user/environment.nix
+++ home/user/environment.nix
@@ -2,6 +2,7 @@
   home.sessionVariables = {
     NIXOS_OZONE_WL = "1";
     WLR_RENDERER_ALLOW_SOFTWARE = "1";
+    WLR_NO_HARDWARE_CURSORS = "1";
 
     # https://wiki.hyprland.org/Configuring/Environment-variables/
     CLUTTER_BACKEND = "waland";
@@ -20,5 +21,8 @@
 
     # Python SSL self signed certs
     REQUESTS_CA_BUNDLE = "/etc/ssl/certs/ca-certificates.crt";
+
+    # game
+    STEAM_EXTRA_COMPAT_TOOLS_PATHS = "\${HOME}/.steam/root/compatibilitytools.d";
   };
 }
--- system/modules/game.nix
+++ system/modules/game.nix
@@ -1,9 +1,17 @@
 {
+  environment.systemPackages = with pkgs; [
+    steam-run
+    protonup
+    mangohud # monitoring FPS, temperatures, CPU/GPU load and more
+    heroic # epic, gog games
+  ];
+
   programs = {
     steam = {
       enable = true;
       remotePlay.openFirewall = true;
     };
+    gamescopeSession.enable = true;
     gamemode.enable = true;
   };
 }
