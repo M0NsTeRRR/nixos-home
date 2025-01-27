76bf8046933eac4a070d82478f2320b2f043d527
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2025-01-27 01:21:34 +0100

    feat: custom ghostty
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- home/user/programs/ghostty.nix
+++ home/user/programs/ghostty.nix
@@ -2,5 +2,14 @@
   programs.ghostty = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      theme = ''
+        {
+          theme = "catppuccin-macchiato";
+          font-size = 12;
+          background-opacity = 0.4;
+        }
+      '';
+    };
   };
 }
