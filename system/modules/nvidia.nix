a076d67303e3f114bf6dcfb303576c51875a06d6
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2025-02-18 23:13:47 +0100

    feat: improve nvidia config
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- system/modules/nvidia.nix
+++ system/modules/nvidia.nix
@@ -1,15 +1,37 @@
-{ config, ... }:
+{ pkgs, config, ... }:
 {
   # Enable NVIDIA
   services.xserver.videoDrivers = [ "nvidia" ];
 
-  hardware.nvidia = {
-    modesetting.enable = true;
-    powerManagement.enable = false;
-    powerManagement.finegrained = false;
-    open = false;
-    nvidiaSettings = true;
-    # https://nixos.wiki/wiki/Nvidia#Running_the_new_RTX_SUPER_on_nixos_stable
-    package = config.boot.kernelPackages.nvidiaPackages.stable;
+  environment.variables = {
+    GBM_BACKEND = "nvidia-drm";
+    LIBVA_DRIVER_NAME = "nvidia";
+    __GLX_VENDOR_LIBRARY_NAME = "nvidia";
+    NVD_BACKEND = "direct";
+  };
+
+  environment.systemPackages = with pkgs; [
+    vulkan-loader
+    vulkan-validation-layers
+    vulkan-tools
+  ];
+
+  hardware = {
+    nvidia = {
+      modesetting.enable = true;
+      powerManagement.enable = true; # Disable if issues with sleep/suspend
+      powerManagement.finegrained = false;
+      open = true;
+      nvidiaSettings = true;
+      # https://nixos.wiki/wiki/Nvidia#Running_the_new_RTX_SUPER_on_nixos_stable
+      package = config.boot.kernelPackages.nvidiaPackages.latest;
+    };
+    graphics = {
+      extraPackages = with pkgs; [
+        nvidia-vaapi-driver
+        vaapiVdpau
+        libvdpau-va-gl
+      ];
+    };
   };
 }
