e493af7f49920f77cc8b951f61d29fbe34ae5c22
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2025-11-04 09:32:49 +0100

    fix(wsl): enable nix-ld

--- hosts/wsl/configuration.nix
+++ hosts/wsl/configuration.nix
@@ -9,6 +9,7 @@
     inputs.nixos-wsl.nixosModules.default
     ../../system/ca-certificates.nix
     ../../system/internationalization.nix
+    ../../system/nix-ld.nix
     ../../system/nixsettings.nix
     ../../system/security.nix
     ../../system/ssh.nix
