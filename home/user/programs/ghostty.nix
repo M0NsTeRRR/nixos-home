76dcc2cb110d0af17c674fbff66b704bc37d1a01
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2026-02-06 00:59:52 +0100

    feat(ghostty): update config
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- home/user/programs/ghostty.nix
+++ home/user/programs/ghostty.nix
@@ -6,7 +6,7 @@
     settings = {
       font-size = 12;
       background-opacity = 0.8;
-      shell-integration-features = "no-cursor,sudo,title,ssh-env,ssh-terminfo";
+      shell-integration-features = "no-cursor,sudo,title,ssh-env";
     };
   };
 }
