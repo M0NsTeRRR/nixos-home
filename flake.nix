c0b7947b2c66e386589f0737295a011bd686fd98
Ludovic Ortega <ludovic.ortega@adminafk.fr>
2026-06-17 21:11:43 +0200

    chore: bump dependencies
    Signed-off-by: Ludovic Ortega <ludovic.ortega@adminafk.fr>

--- flake.nix
+++ flake.nix
@@ -38,7 +38,7 @@
     };
 
     noctalia = {
-      url = "github:noctalia-dev/noctalia-shell";
+      url = "github:noctalia-dev/noctalia/legacy-v4";
       inputs.nixpkgs.follows = "nixpkgs-stable";
     };
 
